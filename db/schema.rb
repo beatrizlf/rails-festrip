e86eee650014b32963fc6a60d85285934dd2528e
Gustavo Silva <guss.silva@hotmail.com>
2021-03-01 19:16:37 -0300

    trip model and route implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_205614) do
+ActiveRecord::Schema.define(version: 2021_03_01_221040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_03_01_205614) do
     t.index ["festival_id"], name: "index_lineups_on_festival_id"
   end
 
+  create_table "trips", force: :cascade do |t|
+    t.bigint "wishlist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["wishlist_id"], name: "index_trips_on_wishlist_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_205614) do
 
   add_foreign_key "lineups", "artists"
   add_foreign_key "lineups", "festivals"
+  add_foreign_key "trips", "wishlists"
   add_foreign_key "wishlists", "festivals"
   add_foreign_key "wishlists", "users"
 end
