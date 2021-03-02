1c80723255b78f29aae984b3085b1fce5ce7107d
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-03-02 18:51:06 -0300

    cloudinary setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :festivals
-  resources :wishlist
+  resources :wishlists
   resources :trips, only: :show
 end
