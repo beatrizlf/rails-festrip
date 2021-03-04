dc451b2095c9c2915d63f93ecf36c06654bbd8ed
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-03-04 20:00:23 -0300

    fixing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,7 @@ class PagesController < ApplicationController
     redirect_to festivals_path if user_signed_in?
   end
 
-<<<<<<< HEAD
   def mochilist
     @wishlists = policy_scope(Wishlist).where(user: current_user).order(created_at: :desc)
-=======
-  def wishlist
-    @wishlists = policy_scope(Wishlist).where(user:current_user).order(created_at: :desc)
->>>>>>> master
   end
 end
