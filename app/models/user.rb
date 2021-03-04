375cdadcc9642c20d0ecc5e5b011dd3589f3616c
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-03-04 15:06:49 -0300

    getting 10 top artists for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
-require 'net/http'
-require 'uri'
-require 'pry'
-
 class User < ApplicationRecord
    # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,44 +6,43 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:spotify]
 
   has_many :wishlists, dependent: :destroy
+  has_many :top_artists, dependent: :destroy
+
   has_one_attached :photo
 
-  #after_create :get_top_artists
-  
+ 
   def self.from_omniauth(auth)
-    user = User.find_by(email: auth.info.email)
-    if user
-      #user.skip_confirmation! 
+    user = where(uid: auth.uid).first_or_create do |user|
       user.uid = auth.uid
-      user.username = auth.info.name
-      user.spotify_photo = auth.info.image
-      return user
-    end
-  
-    where(uid: auth.uid).first_or_create do |user|
-      #user.skip_confirmation!
       user.spotify_token = auth.credentials.token
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.username = auth.uid
       user.spotify_photo = auth.info.image
     end
+    get_top_artists(user)
+    user
   end
 
 
-  #def get_top_artists
-    #@token = self.spotify_token
-    #uri = URI.parse('https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5')
-    #request = Net::HTTP::Get.new(uri)
-    #request['Content-Type'] = 'application/json'
-    #request['Accept'] = 'application/json'
-    #request['Authorization'] = 'Bearer #{@token}'
-    #req_options = {
-      #use_ssl: uri.scheme == 'https',
-    
-    #response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-     # http.request(request)
-    #end
-    #puts response
-    #end
+  def self.get_top_artists(user)
+    token = user.spotify_token
+    response = `curl -X "GET" "https://api.spotify.com/v1/me/top/artists" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer #{token}"`
+    response = JSON.parse(response)
+    user.top_artists.destroy_all
+    response['items'].first(10).each do |item|
+      TopArtist.create(name: item['name'], user: user)
+    end
+   # uri = URI.parse('https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5')
+    # request = Net::HTTP::Get.new(uri)
+    # request['Content-Type'] = 'application/json'
+    # request['Accept'] = 'application/json'
+    # request['Authorization'] = 'Bearer #{@token}'
+    # req_options = {
+    #   use_ssl: uri.scheme == 'https'}
+    # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    #   http.request(request)
+    # end
+    # puts response
+  end
 end
