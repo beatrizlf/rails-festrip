2a1b7818f060f8589a818bba78fc27ceeeea1d1a
Lucas Lima <lucaslbl@outlook.com>
2021-03-04 12:50:29 -0300

    forms and cards fixed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,7 +8,9 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   })
 );
 module.exports = environment
+
