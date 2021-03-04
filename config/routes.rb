6d21f5c8196cc698a22af1ae326a0b07b36afc4d
Gustavo Silva <guss.silva@hotmail.com>
2021-03-04 14:52:53 -0300

    trips view created

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/users/auth/callback', to: 'callbacks#spotify'
 
   root to: 'pages#home'
-  
+
   resources :festivals
   resources :wishlists
   resources :trips, only: :show
