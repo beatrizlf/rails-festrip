6b72ffc841f1eb1c693347dcb931686803c7639f
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-03-07 16:00:12 -0300

    seed with 30 new festivals

--- test/controllers/wishlists_controller_test.rb
+++ test/controllers/wishlists_controller_test.rb
@@ -1,16 +1,6 @@
 require 'test_helper'
 
 class WishlistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get wishlists_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get wishlists_show_url
-    assert_response :success
-  end
-
   test "should get new" do
     get wishlists_new_url
     assert_response :success
