b584c7172ef1cd88c2a556d6e209de7c441381bd
Gustavo Silva <guss.silva@hotmail.com>
2021-03-06 21:37:06 -0300

    artists seed + festivals controller modifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_155336) do
+ActiveRecord::Schema.define(version: 2021_03_06_203041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_155336) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "favourite", default: false
+    t.string "video_url"
   end
 
   create_table "lineups", force: :cascade do |t|
