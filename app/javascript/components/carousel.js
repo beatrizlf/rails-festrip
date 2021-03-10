cb8512b9442b2f7c9011722de945f9318e497462
Lucas Lima <lucaslbl@outlook.com>
2021-03-10 15:43:52 -0300

    owl for artists done

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -1,8 +1,5 @@
 const owlCarousel = () => {
-  $(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
-  });
-  $('.owl-carousel').owlCarousel({
+    $('.owl-four').owlCarousel({
     loop:false,
     margin:2,
     nav:true,
@@ -18,7 +15,23 @@ const owlCarousel = () => {
             items:4
         }
     }
-  })
-}
-
+  });
+  $('.owl-five').owlCarousel({
+      loop:true,
+      margin:1,
+      nav:true,
+      dots:false,
+      responsive:{
+          400: {
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:5
+          }
+      }
+    })
+  }
 export { owlCarousel }
